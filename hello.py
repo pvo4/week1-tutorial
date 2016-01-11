bc8992cc1b8ec3212698b9e45d66c93dc19be37a
Phillip Vo <pvo4@mail.valenciacollege.edu>
2016-01-11 14:22:14 -0500

    Working With Your First Repository: Steps 10-11

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 # replace the word 'hector' with your own name
 
 def hello_world():
-	return 'hello world, my name is Phillip'
+	return 'hello world, my name is '
 
-print( hello_world() + 'hector' )
+print( hello_world() + 'Phillip' )
